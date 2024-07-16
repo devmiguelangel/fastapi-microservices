ad68ec38d26930030cdea23a793ada6e92ba424b
Miguel Angel <miguelangeldev@icloud.com>
2024-07-16 00:42:46 -0400

    feat: add health endpoint

--- services/gateway/main.py
+++ services/gateway/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from api.db.db import close_db_connect, connect_and_init_db
+from api.routers import health
 from api.routers.auth import router as auth_router
 from api.routers.media import router as media_router
 
@@ -9,6 +10,7 @@ app = FastAPI()
 app.add_event_handler('startup', connect_and_init_db)
 app.add_event_handler('shutdown', close_db_connect)
 
+app.include_router(health.router)
 app.include_router(auth_router, prefix='/api/v1')
 app.include_router(media_router, prefix='/api/v1')
 
