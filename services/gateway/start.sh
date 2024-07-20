e0a1d06a43d624e372bb16f5fd938c53a4219e34
Miguel Angel <miguelangeldev@icloud.com>
2024-07-20 16:07:47 -0400

    feat(gateway): update start script

--- services/gateway/start.sh
+++ services/gateway/start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 set -e
-set -x
+set -o errexit
 set -o pipefail
+set -o nounset
+
+while ! nc -z ${DB_HOST} ${DB_PORT}; do
+  echo "Waiting for the mongo database to be ready..."
+  sleep 1
+done
+
+while ! nc -z ${RABBITMQ_HOST} ${RABBITMQ_PORT}; do
+  echo "Waiting for the rabbitmq server to be ready..."
+  sleep 1
+done
 
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
